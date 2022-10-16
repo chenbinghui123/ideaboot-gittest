d98f677bb66d3a51948a803a98c6eef93d10a2a9
cbh <1877975586@qq.com>
2022-10-16 19:46:46 +0800

    dev2 commit

--- src/main/java/com/binghui/ideabootgittest/controller/TestController.java
+++ src/main/java/com/binghui/ideabootgittest/controller/TestController.java
@@ -10,6 +10,7 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
+        System.out.println("dev test!");
 
     }
 }
