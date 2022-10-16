226b115e871ee9a0f720fcff232ecb785090b908
cbh <1877975586@qq.com>
2022-10-16 19:10:55 +0800

    second commit

---
+++ src/main/java/com/binghui/ideabootgittest/controller/Tt.java
@@ -0,0 +1,12 @@
+package com.binghui.ideabootgittest.controller;
+
+/**
+ * @author cbh
+ * @version 1.0
+ * @date 2022/10/16 19:10
+ */
+public class Tt {
+    public static void main(String[] args) {
+
+    }
+}
