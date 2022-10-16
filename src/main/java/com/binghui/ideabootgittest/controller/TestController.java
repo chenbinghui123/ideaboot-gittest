26d75acaecd36bc01ac79f463f4d71f1b94fc52a
cbh <1877975586@qq.com>
2022-10-16 19:47:35 +0800

    master commit

--- src/main/java/com/binghui/ideabootgittest/controller/TestController.java
+++ src/main/java/com/binghui/ideabootgittest/controller/TestController.java
@@ -10,6 +10,7 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
+        System.out.println("master test!");
         System.out.println("dev test!");
         System.out.println("hhhhhhhhhhh test!");
         System.out.println("hhhhhhhhhhh test!");
